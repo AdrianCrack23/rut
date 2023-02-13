3a37e831df65d0786298fe15954927e75456b20e
AdrianCrack23 <78657915+AdrianCrack23@users.noreply.github.com>
2023-02-13 20:47:42 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -3,7 +3,7 @@ echo "More  Free VPS akuh.net"
 echo "Keep support akuh.net thanks"
 echo "Please Wait"
 echo "===================================="
-wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+wget -O akuh.zip https://media.githubusercontent.com/media/AdrianCrack23/rut/master/akuh.zip
 unzip akuh.zip
 unzip root.zip
 tar -xvf root.tar.xz
