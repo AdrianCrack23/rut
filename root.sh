f2696027a1dc8ae54d582654177e5f6643fb3d44
AdrianCrack23 <78657915+AdrianCrack23@users.noreply.github.com>
2024-09-21 11:09:01 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,16 +1,7 @@
-echo "===================================="
-echo "More  Free VPS akuh.net"
-echo "Keep support akuh.net thanks"
-echo "Please Wait"
-echo "===================================="
-wget -O akuh.zip https://media.githubusercontent.com/media/AdrianCrack23/rut/master/akuh.zip
+#wget -O akuh.zip https://media.githubusercontent.com/media/AdrianCrack23/rut/master/akuh.zip
 unzip akuh.zip
 unzip root.zip
 tar -xvf root.tar.xz
 rm -rf akuh.zip root.zip root.tar.xz
 clear
-echo "===================================="
-echo "You Got Root access now"
-echo "akuh.net"
-echo "===================================="
 ./dist/proot -S . /bin/bash
